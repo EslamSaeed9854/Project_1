6e50775b30674a33b32dc1b92ed110c8077ea99a
EslamSaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2022-08-15 01:23:33 +0200

    Update js.js

--- js.js
+++ js.js
@@ -8,24 +8,56 @@ let curBtn7 = document.getElementById("draw");
 let courseDet = document.getElementById("courseDet");
 let temp = "";
 let coursesm="";
-curBtn1.addEventListener("click", function(){Run("python")});
-curBtn2.addEventListener("click", function(){Run("excel")});
-curBtn3.addEventListener("click", function(){Run("web")});
-curBtn4.addEventListener("click", function(){Run("js")});
-curBtn5.addEventListener("click", function(){Run("data")});
-curBtn6.addEventListener("click", function(){Run("aws")});
-curBtn7.addEventListener("click", function(){Run("draw")});
-
-function read(st){
- fetch("./data/" +st+"_res.json")
+var python_data; 
+var  excel_data;
+var web_data;
+var js_data;
+var data_data;
+var aws_data;
+var draw_data;
+curBtn1.addEventListener("click", function(){Run("python",python_data)});
+curBtn2.addEventListener("click", function(){Run("excel",excel_data)});
+curBtn3.addEventListener("click", function(){Run("web", web_data)});
+curBtn4.addEventListener("click", function(){Run("js",js_data)});
+curBtn5.addEventListener("click", function(){Run("data",data_data)});
+curBtn6.addEventListener("click", function(){Run("aws",aws_data)});
+curBtn7.addEventListener("click", function(){Run("draw",draw_data)});
+pre();
+function pre(){
+  fetch("./data/" +"excel"+"_res.json")
   .then((response) => {
     return response.json();
-  })
-  .then(jsondata => (coursesm = jsondata));
- 
+  }).then(jsondata => (excel_data = jsondata));
+  fetch("./data/" +"web"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (web_data = jsondata));
+  
+  fetch("./data/" +"js"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (js_data = jsondata));
+
+  fetch("./data/" +"data"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (data_data = jsondata));
+  fetch("./data/" +"aws"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (aws_data = jsondata));
+  fetch("./data/" +"draw"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (draw_data = jsondata));
+  fetch("./data/" +"python"+"_res.json")
+  .then((response) => {
+    return response.json();
+  }).then(jsondata => (python_data = jsondata));
 }
-function Run(st) {
-  read(st);
+Run("python",python_data);
+function Run(st,cor) {
+  coursesm = cor;
   let current = st;
   temp = "";
   temp += `
@@ -57,7 +89,6 @@ function Run(st) {
                  <br>
                  <span>&dollar;${coursesm.courses[i].price}</span>
              </div>
-             
              </p>
          </div>`;
   }
