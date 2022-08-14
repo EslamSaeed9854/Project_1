a9d1a537306b2358085a2f2a435b54df88421498
EslamSaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2022-08-15 00:10:04 +0200

    .

--- js.js
+++ js.js
@@ -6,10 +6,8 @@ let curBtn5 = document.getElementById("data");
 let curBtn6 = document.getElementById("aws");
 let curBtn7 = document.getElementById("draw");
 let courseDet = document.getElementById("courseDet");
-let jsondata;
-let courses;
-
 let temp = "";
+let coursesm="";
 curBtn1.addEventListener("click", function(){Run("python")});
 curBtn2.addEventListener("click", function(){Run("excel")});
 curBtn3.addEventListener("click", function(){Run("web")});
@@ -18,45 +16,52 @@ curBtn5.addEventListener("click", function(){Run("data")});
 curBtn6.addEventListener("click", function(){Run("aws")});
 curBtn7.addEventListener("click", function(){Run("draw")});
 
+function read(st){
+ fetch("./data/" +st+"_res.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then(jsondata => (coursesm = jsondata));
+ 
+}
 function Run(st) {
-    console.log(st);
-  fetch("./data/" +st+"_res.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((jsondata) => (courses = jsondata));
+  read(st);
   let current = st;
   temp = "";
   temp += `
-     <strong style="font-size: 30px; line-height: 2.5">${courses.header}<br></strong>
-     <span> ${courses.description}<br /><br/></span>
+     <strong style="font-size: 30px; line-height: 2.5">${coursesm.header}<br></strong>
+     <span> ${coursesm.description}<br/><br/></span>
      <button class="expore-pyth">Explore ${current}</button>
      <div class="Courses-photos">
      <!--Cources-photos-->
      `;
-  for (var i = 0; i < courses.courses.length; i++) {
+  for (var i = 0; i < coursesm.courses.length; i++) {
     temp += `
          <div class="one-course">
-             <img src="${courses.courses[i].image}" alt="" />
+             <img src="${coursesm.courses[i].image}" alt="" />
              <p>
              <strong>
-                 ${courses.courses[i].title}
+                 ${coursesm.courses[i].title}
+                 <br>
              </strong>
              <strong>
-               
+             ${coursesm.courses[i].instructors[0].name}<br>
              </strong>
              <div>
-                 <span >${courses.courses[i].rating}</span>
+                 <span >${coursesm.courses[i].rating}</span>
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star"></span>
                  <span class="fa fa-star"></span>
+                 <br>
+                 <span>&dollar;${coursesm.courses[i].price}</span>
              </div>
-             <span>&dollar;${courses.courses[i].price}</span>
+             
              </p>
          </div>`;
   }
+
   temp += "</div>";
   courseDet.innerHTML = temp;
 }
