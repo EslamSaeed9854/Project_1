0c54e443d17d0e9696325119a0b7a83d158ba5bb
EslamSaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2022-08-15 03:04:01 +0200

    .

--- js.js
+++ js.js
@@ -6,6 +6,9 @@ let curBtn5 = document.getElementById("data");
 let curBtn6 = document.getElementById("aws");
 let curBtn7 = document.getElementById("draw");
 let courseDet = document.getElementById("courseDet");
+let search_txt = document.getElementById("in-search");
+let search_photo = document.getElementById("search_photo");
+let links = document.getElementsByClassName("button-style");
 let temp = "";
 let coursesm="";
 var python_data; 
@@ -15,14 +18,43 @@ var js_data;
 var data_data;
 var aws_data;
 var draw_data;
-curBtn1.addEventListener("click", function(){Run("python",python_data)});
-curBtn2.addEventListener("click", function(){Run("excel",excel_data)});
-curBtn3.addEventListener("click", function(){Run("web", web_data)});
-curBtn4.addEventListener("click", function(){Run("js",js_data)});
-curBtn5.addEventListener("click", function(){Run("data",data_data)});
-curBtn6.addEventListener("click", function(){Run("aws",aws_data)});
-curBtn7.addEventListener("click", function(){Run("draw",draw_data)});
-pre();
+search_photo.addEventListener("click" , function(){
+  if(search_txt.value == "python"){
+    Run(search_txt.value,python_data);
+    curBtn1.classList.add("active")
+  }
+  if(search_txt.value == "excel"){
+    Run(search_txt.value,excel_data);
+    curBtn2.classList.add("active")
+  }
+  if(search_txt.value == "web"){
+    Run(search_txt.value,web_data);
+    curBtn3.classList.add("active")
+  }
+  if(search_txt.value == "javascript"){
+    Run(search_txt.value,js_data);
+    curBtn4.classList.add("active")
+  }
+  if(search_txt.value == "datascience"){
+    Run(search_txt.value,data_data);
+    curBtn5.classList.add("active")
+  }
+  if(search_txt.value == "AWS"){
+    Run(search_txt.value,aws_data);
+    curBtn6.classList.add("active")
+  }
+  if(search_txt.value == "draw"){
+    Run(search_txt.value,draw_data);
+    curBtn7.classList.add("active")
+  }
+});
+curBtn1.addEventListener("click", function(){Run("python",python_data);curBtn1.classList.add("active")});
+curBtn2.addEventListener("click", function(){Run("excel",excel_data);curBtn2.classList.add("active")});
+curBtn3.addEventListener("click", function(){Run("web", web_data);curBtn3.classList.add("active")});
+curBtn4.addEventListener("click", function(){Run("js",js_data);curBtn4.classList.add("active")});
+curBtn5.addEventListener("click", function(){Run("data",data_data);curBtn5.classList.add("active")});
+curBtn6.addEventListener("click", function(){Run("aws",aws_data);curBtn6.classList.add("active")});
+curBtn7.addEventListener("click", function(){Run("draw",draw_data);curBtn7.classList.add("active")});
 function pre(){
   fetch("./data/" +"excel"+"_res.json")
   .then((response) => {
@@ -55,8 +87,8 @@ function pre(){
     return response.json();
   }).then(jsondata => (python_data = jsondata));
 }
-Run("python",python_data);
 function Run(st,cor) {
+  removeAct();
   coursesm = cor;
   let current = st;
   temp = "";
@@ -95,4 +127,16 @@ function Run(st,cor) {
 
   temp += "</div>";
   courseDet.innerHTML = temp;
+  return 1;
 }
+function removeAct()
+{
+  for(let i = 0 ; i < links.length ; i++)
+  {
+    links[i].classList.remove("active");
+  }
+}
+setTimeout(function () {
+  pre();}, 0); 
+  setTimeout(function () {
+    Run("python",python_data);curBtn1.classList.add("active")}, 555);
